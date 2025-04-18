ed2840011d0172f1c31c46b3010f3316c7866f26
ilkeryasinkoc <ilkerkoc2003@gmail.com>
2025-04-18 16:27:11 +0200

    Update sycript.js

--- sycript.js
+++ sycript.js
@@ -170,15 +170,6 @@ document.addEventListener("DOMContentLoaded", function () {
     checkScroll();
 });
 
-window.addEventListener("scroll", function () {
-    const navbar = document.getElementById("navbar");
-    if (window.scrollY > 50) {
-        navbar.classList.add("navbar-scroll");
-    } else {
-        navbar.classList.remove("navbar-scroll");
-    }
-});
-
 document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.getElementById("menu-toggle");
     const mobileNav = document.getElementById("mobile-nav");
